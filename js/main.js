df3478913ceb2c685907d7069644603590457df4
kulotsystems <kulotsystems@gmail.com>
2023-09-12 15:04:30 +0800

    Simulate other async requests.

--- js/main.js
+++ js/main.js
@@ -11,8 +11,7 @@ function poll()
 
     fetch('app/index.php', {
         method: 'post',
-        body: form,
-
+        body: form
     })
         .then(res => res.json())
         .then(data => {
@@ -28,6 +27,40 @@ function poll()
             poll();
         });
 }
+
+// another async request
+function asyncRequest(btn, span) {
+    // form data
+    const form = new FormData();
+    form.append('epoch', 1);
+
+    btn.disabled = true;
+    fetch('app/index.php', {
+        method: 'post',
+        body: form
+    })
+        .then(res => res.json())
+        .then(data => {
+            span.innerHTML = data.timestamp;
+        })
+        .catch(err => {
+            console.log("Async Request Error: ", err);
+        })
+        .finally(() => {
+            btn.disabled = false;
+        });
+}
+
+const btnAsync1 = document.querySelector('#btn-async1');
+const btnAsync2 = document.querySelector('#btn-async2');
+const bAsync1  = document.querySelector('#b-async1');
+const bAsync2  = document.querySelector('#b-async2');
+btnAsync1.addEventListener('click', () => {
+    asyncRequest(btnAsync1, bAsync1);
+});
+btnAsync2.addEventListener('click', () => {
+    asyncRequest(btnAsync2, bAsync2);
+});
 
 
 // run poll onload
